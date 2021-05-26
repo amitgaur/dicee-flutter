d3db560c03f02a68d6c31a573412df9abb15bb33
Amit Guar <amitgaur@gmail.com>
2021-05-26 11:29:58 -0700

    get it all working

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,66 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  return runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
-        ),
-        body: DicePage(),
-      ),
-    ),
-  );
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
 }
 
-class DicePage extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  //int _left, _right = 1;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      //mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Expanded(flex: 10, child: Image.asset('images/dice1.png')),
-        Expanded(flex: 5, child: Image.asset('images/dice2.png'))
-      ],
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.purpleAccent,
+      appBar: AppBar(
+        title: Text('Dicee'),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+      body: MyBody(),
+    ));
+  }
+}
+
+class MyBody extends StatefulWidget {
+  @override
+  _MyBodyState createState() => _MyBodyState();
+}
+
+class _MyBodyState extends State<MyBody> {
+  var _left = 1, _right = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              flex: 1,
+              child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      _left = Random().nextInt(5) + 1;
+                      print('AppBrew left state update $_left');
+                    });
+                  },
+                  child: Image.asset('images/dice$_left.png'))),
+          Expanded(
+              flex: 1,
+              child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      _right = Random().nextInt(5) + 1;
+                      print('AppBrew right state update $_right');
+                    });
+                  },
+                  child: Image.asset('images/dice$_right.png')))
+        ],
+      ),
     );
   }
 }
